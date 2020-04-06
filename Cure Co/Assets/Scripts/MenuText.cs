142beb1ae86f786ade0a171cba981797fd6834b7
pankow602 <zzrhydonzz1@gmail.com>
2020-04-06 14:38:52 -0400

    Sprint 1 Script initialization

--- Cure Co/Assets/Scripts/MenuText.cs
+++ Cure Co/Assets/Scripts/MenuText.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
 
-public class MenuText : MonoBehaviour
+public class ButtonControl, IPointerEnterHandler, IPointerExitHandler 
+ {
+     private GameObject childText = null; //  or make public and drag
+void Start()
 {
-    // Start is called before the first frame update
-    void Start()
+    Text text = GetComponentInChildren<Text>();
+    if (text != null)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        childText = text.gameObject;
+        childText.SetActive(false);
     }
 }
+public void OnPointerEnter(EventSystems.PointerEventData eventData)
+{
+    childText.SetActive(true);
+}
+public void OnPointerExit(EventSystems.PointerEventData eventData)
+{
+    childText.SetActive(false);
+}
+ }
+
