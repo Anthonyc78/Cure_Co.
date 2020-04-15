71c0eac4a54735203932647c3434e299f1fbd943
brenyork <60360742+brenyork@users.noreply.github.com>
2020-04-15 16:06:02 -0400

    Added start menu and button controller.

---
+++ Assets/Scripts/MenuButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuButtonController : MonoBehaviour
+{
+    [SerializeField] GameObject targetObject;
+    [SerializeField] GameObject SetMeActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void SetGOActive()
+    {
+        SetMeActive.SetActive(true);
+    }
+    private void OnMouseOver()
+    {
+        targetObject.SetActive(true);
+    }
+    private void OnMouseEnter()
+    {
+        targetObject.SetActive(true);
+    }
+    private void OnMouseExit()
+    {
+        targetObject.SetActive(false);
+    }
+}
