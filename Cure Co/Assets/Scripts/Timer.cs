fdd53a5e0fbfc31a1eb24df50d8b1d4ab76e451d
Anthony Christy <christ422@morrisville.edu>
2020-05-11 15:51:33 -0400

    pause timer script created

---
+++ Cure Co/Assets/Scripts/Timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float gameTimer;
+    public bool gamePaused = false;
+
+    void Update()
+    {
+        if (!gamePaused)
+        {
+            gameTimer += Time.deltaTime;
+        }
+    }
+}
+// https://forum.unity.com/threads/how-do-i-pause-a-timer.165939/
